163be612e34f69ed4d6287d951d2ac43deee5e19
Neel Patel <neelpatel@Neels-MacBook-Air.local>
2018-11-12 15:19:50 -0600

    modified vector graphics, text, and links

--- frontend/src/common/components/footer/footer.component.tsx
+++ frontend/src/common/components/footer/footer.component.tsx
@@ -6,14 +6,14 @@ const style = require("./footer.style.scss");
 
 const Links = () => (
   <div className={style.linkArea}>
-    <a className={style.link} href="https://technet.microsoft.com/en-US/cc300389.aspx" target="__blank">Terms of Use</a>
-    <a className={style.link} href="https://go.microsoft.com/fwlink/?LinkId=248681" target="__blank">Privacy</a>    
+    <a className={style.link} href="#" target="__blank">Terms of Use</a>
+    <a className={style.link} href="#" target="__blank">Privacy</a>    
   </div>
 );
 
 const Statement = () => (
   <div className={style.statementArea}>
-    © Microsoft 2018
+    © Yash Technologies 2018
   </div>
 );
 
@@ -21,7 +21,7 @@ export const FooterComponent = ({className = null}) => {
   return (
     <footer className={cnc(style.footer, className)}>
       <Statement />
-      <a href="https:\\www.microsoft.com" target="__blank">
+      <a href="https:\\www.yash.com" target="__blank">
         <LogoMicrosoftComponent colorful={false} classes={{container: style.logoContainer, svg: style.logoSvg }} />
       </a> 
       <Links />
